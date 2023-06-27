f24533d551c4cad0ed2101291e629b26f4c9c0ab
Nadav Man <nad.man926@gmail.com>
2023-06-27 16:06:53 +0300

    use data file if its from last week

--- src/nadios_bot/main.py
+++ src/nadios_bot/main.py
@@ -2,6 +2,7 @@ from nadios_bot.consts import TELEGRAM_BOT_API_KEY, PREVIOUS_SHOWS_PATH, \
     REGISTERED_USERS_PATH, REGISTERED_USERS_DEFAULT_VALUE, LOG_FILE_PATH, PREVIOUS_SHOWS_DEFAULT_VALUE
 from nadios_bot.telegram_utils import run_bot, get_all_events, save_events
 from logging.handlers import RotatingFileHandler
+import datetime
 import logging
 import json
 import os
@@ -26,8 +27,22 @@ def setup_log():
 def setup_data_file(file_path, file_default_data):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     if not os.path.exists(file_path):
+        logging.info(f"Can't find {file_path} - Creating it.")
         with open(file_path, 'w') as f:
             json.dump(file_default_data, f)
+        return True
+    return False
+
+
+def setup_events_data_file(file_path, file_default_data):
+    just_created = setup_data_file(file_path=file_path, file_default_data=file_default_data)
+    days_since_modify = (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getmtime(file_path))).days
+    if days_since_modify > 7 and not just_created:
+        logging.info(f"{file_path} is to old - Override his content.")
+        save_events(get_all_events())
+    else:
+        logging.info(f"The data file already exist.")
+
 
 
 def setup_bot():
@@ -35,8 +50,8 @@ def setup_bot():
     if TELEGRAM_BOT_API_KEY is None:
         raise MissingEnvironmentVariable(f"TELEGRAM_BOT_API_KEY does not exist")
     setup_data_file(file_path=REGISTERED_USERS_PATH, file_default_data=REGISTERED_USERS_DEFAULT_VALUE)
-    setup_data_file(file_path=PREVIOUS_SHOWS_PATH, file_default_data=PREVIOUS_SHOWS_DEFAULT_VALUE)
-    save_events(get_all_events())
+    setup_events_data_file(file_path=PREVIOUS_SHOWS_PATH, file_default_data=PREVIOUS_SHOWS_DEFAULT_VALUE)
+
     logging.info("Finish setup successfully!")
 
 
